1a31ecd91da71163d04f2e41b55cbf1abe8f2fa8
IbaiBuR <ibaibur@chessnibble.com>
2024-05-30 23:48:47 +0200

    refactor: Align stats output and pass reboots by const reference

--- cpp/src/utils.hpp
+++ cpp/src/utils.hpp
@@ -10,21 +10,21 @@
 
 namespace utils
 {
-    inline auto print_stats(std::array<u8, RUNS>& reboots) -> void
+    inline auto print_stats(const std::array<u8, RUNS>& reboots) -> void
     {
         const auto total_reboots = std::accumulate(reboots.begin(), reboots.end(), 0);
-        const auto avg_reboots = total_reboots / reboots.size();
+        const auto avg_reboots = static_cast<double>(total_reboots) / static_cast<double>(reboots.size());
         const auto min_reboots = *std::ranges::min_element(reboots.begin(), reboots.end());
         const auto max_reboots = *std::ranges::max_element(reboots.begin(), reboots.end());
 
-        std::println("\n=====================================");
-        std::println("||           STATISTICS            ||");
-        std::println("=====================================");
-        std::println("|| Total number of reboots: {:6} ||", total_reboots);
-        std::println("|| Average number of reboots: {:4} ||", avg_reboots);
-        std::println("|| Maximum number of reboots: {:4} ||", max_reboots);
-        std::println("|| Minimum number of reboots: {:4} ||", min_reboots);
-        std::println("=====================================");
+        std::println("\n=========================================");
+        std::println("||             STATISTICS              ||");
+        std::println("=========================================");
+        std::println("|| Total   number of reboots: {:7}  ||", total_reboots);
+        std::println("|| Average number of reboots: {:7.5f}  ||", avg_reboots);
+        std::println("|| Maximum number of reboots: {:7}  ||", max_reboots);
+        std::println("|| Minimum number of reboots: {:7}  ||", min_reboots);
+        std::println("=========================================");
     }
 
     inline auto getTimeMs() -> long
