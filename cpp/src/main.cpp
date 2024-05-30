f4d270997d7d57d65abff50d278e1bd41e7108f7
IbaiBuR <ibaibur@chessnibble.com>
2024-05-30 23:49:30 +0200

    refactor: Calculate elapsed time before printing stats

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -8,18 +8,18 @@ inline auto run(std::array<u8, RUNS>& reboots) -> void
         while (!box::select<false>(box::init()))
             ++reboots[i];
     }
-
-    utils::print_stats(reboots);
 }
 
 int main()
 {
     std::array<u8, RUNS> reboots{};
+
     const auto start = utils::getTimeMs();
-
     run(reboots);
+    const auto elapsed = utils::getTimeMs() - start;
 
-    std::println("\nElapsed time: {} ms", utils::getTimeMs() - start);
+    utils::print_stats(reboots);
+    std::println("\n\033[32mElapsed time: {} ms\033[0m", elapsed);
 
     return 0;
 }
