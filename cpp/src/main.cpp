5bfe446d019823301450c6359c4fe124fd65491f
IbaiBuR <ibaibur@chessnibble.com>
2024-06-18 23:54:20 +0200

    feat: Add parallel version using the C++ STL

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,8 +3,10 @@
 int main()
 {
     run::single_threaded();
+
     run::multi_threaded::manual();
     run::multi_threaded::thread_pool();
+    run::multi_threaded::parallel_std();
 
     return 0;
 }
--- cpp/src/run.hpp
+++ cpp/src/run.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <execution>
 #include <thread>
 #include <vector>
 
@@ -85,6 +86,29 @@ namespace run
             const auto elapsed = utils::get_time_ms() - start_time;
 
             utils::print_stats(reboots);
+            std::println("\n\033[32mElapsed time: {} ms\033[0m", elapsed);
+        }
+
+        inline auto parallel_std() -> void
+        {
+            std::vector<std::atomic<u8>> reboots(default_runs);
+
+            const auto start_time = utils::get_time_ms();
+
+            std::for_each(std::execution::par, reboots.begin(), reboots.end(), [](std::atomic<u8>& reboot)
+            {
+                thread_local std::mt19937 rng(std::random_device{}());
+
+                while (!box::select<false>(box::init(rng)))
+                    reboot.fetch_add(1, std::memory_order::relaxed);
+            });
+
+            const auto elapsed = utils::get_time_ms() - start_time;
+
+            std::vector<u8> copied_reboots(default_runs);
+            std::ranges::copy(reboots.begin(), reboots.end(), copied_reboots.begin());
+
+            utils::print_stats(copied_reboots);
             std::println("\n\033[32mElapsed time: {} ms\033[0m", elapsed);
         }
     }
